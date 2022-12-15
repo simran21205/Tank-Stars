24ecf205e58a6f2872e5cdbc5a6a4b26b1cc293e
nikita21546 <108211279+nikita21546@users.noreply.github.com>
2022-12-15 22:10:07 +0530

    Update WeaponsScreen.java

--- core/build/classes/java/main/com/mygdx/game/Screens/WeaponsScreen.java
+++ core/build/classes/java/main/com/mygdx/game/Screens/WeaponsScreen.java
@@ -1,181 +1,95 @@
-package com.mygdx.game.Screens;
+package com.mygdx.game;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.Interpolation;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.Screens.*;
+import com.mygdx.game.Screens.LoadingScreen;
+//import com.mygdx.game.Screens.PlayScreen;
+import com.mygdx.game.Screens.SplashScreen;
 
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
-import com.mygdx.game.TankStars;
+public class TankStars extends Game {
 
-import static com.badlogic.gdx.scenes.scene2d.actions.Actions.*;
+    public static final float VERSION = .8f;
+    public static final int V_WIDTH = 800;
+    public static final int V_HEIGHT = 480;
 
-public class WeaponsScreen implements Screen {
+    public OrthographicCamera camera;
+    public SpriteBatch batch;
 
-    private final TankStars app;
-
-    private Stage stage;
-    private Skin skin;
-
-    private TextButton buttonSettings, buttonWeapon ;
-    private ShapeRenderer shapeRenderer;
-    private Image splashImg;
-    private TextButton.TextButtonStyle style;
-
-    public WeaponsScreen(final TankStars app) {
-        this.app = app;
-        this.stage = new Stage(new FitViewport(TankStars.V_WIDTH, TankStars.V_HEIGHT, app.camera));
-        this.shapeRenderer = new ShapeRenderer();
-    }
+    public BitmapFont font;
+//
+    public AssetManager assets;
+//
+    public LoadingScreen loadingScreen;
+    public SplashScreen splashScreen;
+    public MainMenuScreen mainMenuScreen;
+    public ChooseTank chooseTankScreen;
+    public ChooseTank2 chooseTankScreen2;
+    public MainToChoose mainToChoose;
+    public PlayScreen playScreen;
+    public SettingsScreen settingsScreen;
+    public WeaponsScreen weaponsScreen;
 
     @Override
-    public void show() {
-        System.out.println("WEAPONS MENU");
-        Gdx.input.setInputProcessor(stage);
-        stage.clear();
+    public void create() {
+        assets = new AssetManager();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, V_WIDTH, V_HEIGHT);
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        font.setColor(Color.BLACK);
+//        this.setScreen(new SplashScreen(this));
 
-        Texture splashTex = app.assets.get("weapons.png", Texture.class);
-        splashImg = new Image(splashTex);
-        splashImg.setOrigin(splashImg.getWidth() / 2 , splashImg.getHeight() / 2 );
-        splashImg.setPosition(stage.getWidth() / 2 -400, stage.getHeight()/2 -240);
-        splashImg.addAction(sequence(alpha(0), scaleTo(1f, 1f), parallel(fadeIn(0f),
-                scaleTo(1f, 1f, 2f),
-                moveTo(stage.getWidth() / 2 -400, stage.getHeight() / 2 -240, 2f))));
-        stage.addActor(splashImg);
-
-        this.skin = new Skin();
-        this.skin.addRegions(app.assets.get("ui/uiskin.atlas", TextureAtlas.class));
-        this.skin.add("default-font", app.font);
-        this.skin.load(Gdx.files.internal("ui/uiskin.json"));
-
-        initButtons();
+//        initFonts();
+//
+        loadingScreen = new LoadingScreen(this);
+        splashScreen = new SplashScreen(this);
+        mainMenuScreen = new MainMenuScreen(this);
+        chooseTankScreen = new ChooseTank(this);
+        chooseTankScreen2 = new ChooseTank2(this);
+        mainToChoose = new MainToChoose(this);
+        playScreen = new PlayScreen(this);
+        settingsScreen = new SettingsScreen(this);
+        weaponsScreen = new WeaponsScreen(this);
+//
+        this.setScreen(loadingScreen);
     }
 
-    private void update(float delta) {
-        stage.act(delta);
-    }
 
     @Override
-    public void render(float delta) {
-        Gdx.gl.glClearColor(0,0,0,1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    public void render() {
+        super.render();
 
-        update(delta);
-        app.batch.begin();
-        app.batch.end();
-
-
-        stage.draw();
-    }
-
-    @Override
-    public void resize(int width, int height) {
-        stage.getViewport().update(width, height, true);
-    }
-
-    @Override
-    public void pause() {
-
-    }
-
-    @Override
-    public void resume() {
-
-    }
-
-    @Override
-    public void hide() {
-
+//        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+//            Gdx.app.exit();
+//        }
     }
 
     @Override
     public void dispose() {
-        stage.dispose();
-        shapeRenderer.dispose();
+        batch.dispose();
+        font.dispose();
+        assets.dispose();
+        loadingScreen.dispose();
+        splashScreen.dispose();
+        mainMenuScreen.dispose();
+        chooseTankScreen.dispose();
+        chooseTankScreen2.dispose();
+//        playScreen.dispose();
     }
 
-    private void initButtons() {
+    private void initFonts() {
+//        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/Arcon.ttf"));
+//        FreeTypeFontGenerator.FreeTypeFontParameter params = new FreeTypeFontGenerator.FreeTypeFontParameter();
 //
-//        buttonNew = new TextButton("Tank1", skin, "default");
-//        buttonNew.setPosition(55, 45);
-//        buttonNew.setSize(185, 70);
-////        buttonNew.addAction(sequence(alpha(0), parallel(fadeIn(.5f), moveBy(0, -20, .5f, Interpolation.pow5Out))));
-//        buttonNew.addListener(new ClickListener() {
-//            @Override
-//            public void clicked(InputEvent event, float x, float y) {
-//                app.setScreen(app.chooseTankScreen2);
-//            }
-//        });
-//
-//        buttonResume = new TextButton("Tank2", skin, "default");
-//        buttonResume.setPosition(315, 45);
-//        buttonResume.setSize(185, 70);
-////        buttonResume.addAction(sequence(alpha(0), parallel(fadeIn(.5f), moveBy(0, -20, .5f, Interpolation.pow5Out))));
-//        buttonResume.addListener(new ClickListener() {
-//            @Override
-//            public void clicked(InputEvent event, float x, float y) {
-//                app.setScreen(app.chooseTankScreen2);
-//            }
-//        });
-//
-//        buttonExit = new TextButton("Tank3", skin, "default");
-//        buttonExit.setPosition(571, 45);
-//        buttonExit.setSize(185 , 70);
-////        buttonExit.addAction(sequence(alpha(0), parallel(fadeIn(.5f), moveBy(0, -20, .5f, Interpolation.pow5Out))));
-//        buttonExit.addListener(new ClickListener() {
-//            @Override
-//            public void clicked(InputEvent event, float x, float y) {
-//                app.setScreen(app.chooseTankScreen2);
-//            }
-//        });
-//
-//        buttonBack = new TextButton("Back", skin, "default");
-//        buttonBack.setPosition(35, 375);
-//        buttonBack.setSize(60, 60);
-////        buttonBack.addAction(sequence(alpha(0), parallel(fadeIn(.5f), moveBy(0, -20, .5f, Interpolation.pow5Out))));
-//        buttonBack.addListener(new ClickListener() {
-//            @Override
-//            public void clicked(InputEvent event, float x, float y) {
-//                app.setScreen(app.mainMenuScreen);
-//            }
-//        });
-        buttonWeapon = new TextButton("W", skin, "default");
-        buttonWeapon.setPosition(103, 38);
-        buttonWeapon.setSize(50 , 50);
-//        buttonExit.addAction(sequence(alpha(0), parallel(fadeIn(.5f), moveBy(0, -20, .5f, Interpolation.pow5Out))));
-        buttonWeapon.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                app.setScreen(app.playScreen);
-            }
-        });
-//
-        buttonSettings = new TextButton("Settings", skin, "default");
-        buttonSettings.setPosition(38, 372);
-        buttonSettings.setSize(60, 60);
-//        buttonBack.addAction(sequence(alpha(0), parallel(fadeIn(.5f), moveBy(0, -20, .5f, Interpolation.pow5Out))));
-        buttonSettings.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                app.setScreen(app.settingsScreen);
-            }
-        });
-        stage.addActor(buttonSettings);
-        stage.addActor(buttonWeapon);
-//        stage.addActor(buttonResume);
-//        stage.addActor(buttonNew);
-//        stage.addActor(buttonExit);
+//        params.size = 24;
+//        params.color = Color.BLACK;
+//        font24 = generator.generateFont(params);
     }
 }
