794e807ada1e70e3fcb5ba493453117420cc806d
simran21205 <108448487+simran21205@users.noreply.github.com>
2022-12-18 20:09:05 +0530

    Create PlayerTank.java

---
+++ PlayerTank.java
@@ -0,0 +1,24 @@
+package com.mygdx.game.Screens;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class PlayerTank extends Tank{
+    public PlayerTank(float xCentre, float yCentre,
+                      float width, float height,
+                      float movementSpeed, int sheild,
+                      float weapon_width, float weapon_heigth, float weaponMovementspeed,
+                      float timeBetweenshots, TextureRegion shipTexture,
+                      TextureRegion shieldTexture, TextureRegion weaponsTexture) {
+        super(xCentre, yCentre, width, height, movementSpeed, sheild, weapon_width, weapon_heigth, weaponMovementspeed, timeBetweenshots, shipTexture, shieldTexture, weaponsTexture);
+    }
+
+    @Override
+    public Weapons[] fireweapons() {
+        Weapons[] weapons = new Weapons[1];
+        weapons[0]=new Weapons(xPosition*width*0.82f,yPosition*height*0.45f,
+                weapon_width,weapon_heigth,
+                weaponMovementspeed,weaponsTextureRegion);
+        timesinceLastshot=0;
+        return weapons;
+    }
+}
