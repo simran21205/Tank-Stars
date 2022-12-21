25eb4fc424e14ed0f3c2e2a1968ed8af443d824a
simran21205 <108448487+simran21205@users.noreply.github.com>
2022-12-21 23:31:07 +0530

    Update GameScreen8.java

--- GameScreen8.java
+++ GameScreen8.java
@@ -445,7 +445,7 @@ public class GameScreen8 extends InputAdapter implements Screen, Json.Serializab
                     app.setScreen(new MainMenuScreen(app));
                 }
                 else if (Gdx.input.isKeyPressed(Input.Keys.S)) {
-                    app.loadGameScreen.L=1;
+                    app.loadGameScreen.L=8;
                     app.setScreen(app.mainMenuScreen);
 
                 }
